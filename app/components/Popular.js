edf8a6aa139940530ccd9167fa0081cda5b13e0a
Gizem Ocak <Gizem@Gizems-MacBook-Air.local>
2018-10-17 13:15:45 -0400

    second commit

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -14,7 +14,7 @@ function SelectLanguage(props) {
               }
               onClick={() => {
                 props.onSelect(lang);
-                props.fetchData();
+                props.onFetchData(lang);
               }}
               key={lang}
             >
@@ -26,6 +26,38 @@ function SelectLanguage(props) {
     </div>
   );
 }
+
+function RepoGrid(props) {
+  return (
+    <ul className="popular-list">
+      {props.repos.map((repo, index) => {
+        return (
+          <li key={repo.name} className="popular-item">
+            <div className="popular-rank">#{index + 1}</div>
+            <ul className="space-list-items">
+              <li>
+                <img
+                  className="avatar"
+                  src={repo.owner.avatar_url}
+                  alt={"Avatar for" + repo.owner.login}
+                />
+              </li>
+              <li>
+                <a href={repo.html_url}>{repo.name}</a>
+              </li>
+              <li>@{repo.owner.login}</li>
+              <li>{repo.stargazers_count} stars</li>
+            </ul>
+          </li>
+        );
+      })}
+    </ul>
+  );
+}
+
+RepoGrid.popTypes = {
+  repos: PropTypes.array.isRequired
+};
 
 SelectLanguage.propTypes = {
   selectedLanguage: PropTypes.string.isRequired,
@@ -45,17 +77,15 @@ class Popular extends React.Component {
     this.fetchData(this.state.selectedLanguage);
   }
 
-  fetchData() {
-    console.log();
-    fetch(
-      "https://api.github.com/search/repositories?q=stars:>1+language:" +
-        this.state.selectedLanguage +
-        "&sort=stars&order=desc&type=Repositories"
-    )
+  fetchData = (language = "All") => {
+    const url = `https://api.github.com/search/repositories?q=stars:>1+language:
+    ${language}
+    &sort=stars&order=desc&type=Repositories`;
+    fetch(url)
       .then(response => response.json())
       .then(data => this.setState({ repos: data.items }))
       .catch(error => console.error("Error:", error));
-  }
+  };
 
   updateLanguage(lang) {
     this.setState({ selectedLanguage: lang });
@@ -65,10 +95,15 @@ class Popular extends React.Component {
     return (
       <div>
         <SelectLanguage
-          onClick={this.fetchData()}
+          onFetchData={this.fetchData}
           selectedLanguage={this.state.selectedLanguage}
           onSelect={lang => this.updateLanguage(lang)}
         />
+        {!this.state.repos ? (
+          <p>LOADING...</p>
+        ) : (
+          <RepoGrid repos={this.state.repos} />
+        )}
       </div>
     );
   }
